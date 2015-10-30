87658474c22de827d670c3c4cd290281da2bb8ad
Ahmed <ahmed.it.hmoud@gmail.com>
2015-10-30 13:18:51 -0700

    Fix the layout style Replace refId with instanceId Fix slider destroy issues

--- js/shared.js
+++ js/shared.js
@@ -7,8 +7,8 @@ folderPluginShared.getPluginDetails = function (pluginsInfo, pluginIds) {
     for (var id = 0; id < pluginIds.length; id++) {
         for (var i = 0; i < pluginsInfo.length; i++) {
             tempPlugin = {};
-            if (pluginIds[id] == pluginsInfo[i].data.refId) {
-                tempPlugin.instanceId = pluginsInfo[i].data.refId;
+            if (pluginIds[id] == pluginsInfo[i].data.instanceId) {
+                tempPlugin.instanceId = pluginsInfo[i].data.instanceId;
                 if (pluginsInfo[i].data) {
                     tempPlugin.iconUrl = pluginsInfo[i].data.iconUrl;
                     tempPlugin.title = pluginsInfo[i].data.title;
